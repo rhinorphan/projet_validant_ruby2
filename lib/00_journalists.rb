f00081847385f853b4dac5745a671bab09bb5c4a
Axel Tarifa <axel.tarifa@gmail.com>
2021-04-15 15:43:44 +0200

    second commit // final  00_journalists.rb

---
+++ lib/00_journalists.rb
@@ -0,0 +1,108 @@
+def data
+  journalists = ["@jcunniet","@PaulLampon","@Aziliz31","@ssoumier","@marionsouzeau","@gaellombart","@bendarag","@AurelieLebelle","@julienduffe","@thomaspoupeau","@LilyRossignol","@ClairGuedon","@stephanieauguy","@claw_prolongeau","@_JulieMenard","@LColcomb","@Zlauwereys","@MeLonguet","@DorotheeLN","@NolwennCosson","@ADaboval","@Remibaldy","@bderveaux","@amandechap","@ELODIESOULIE","@nbongarcon","@HeloAb","@Panamorama","@gregplou","@BenoitBerthe","@LauraBruneau89","@Anthony_Lieures","@Sharonwaj","@mcsonkin","@pverduzier","@emiliel3","@Julien_MARION","@SophiFay","@bdelombre","@annalecerf","@AdriaBudry","@DejNikolic","@iJaffre","@CyrusleVirus","@GPardigon","@e_vallerey","@IsabelleSouquet","@AudeDavidRossi","@Yoann_Pa","@CeliaPenavaire","@perraultvincent","@cboulate","@JustineWeyl","@Paulinejacot","@juliechab","@aslechevallier","@phnou","@Seb_Pommier","@Alex_Bensaid","@GuillaumeGaven","@annelaurechouin","@Oliviader","@guerricp","@JMMarchaut","@cyceron","@gregory_raymond","@vhunsinger","@l_peillon","@fannyguinochet","@EAssayag","@KibweAdom","@YvonHerry","@JohanGarciajg","@saidlabidi","@lauranneprov","@LeaDavy","@francois_remy","@CGuicheteau","@FloMaillet","@m_perroud","@oBrunet_TSMF","@MoonVdc","@jc2taille","@NellyMoussu","@VirginK","@b_misa","@FabriceCouste","@barbara_prose","@lelia2m","@brunoaskenazi","@laurenechamp","@ysisbox","@juliengagliardi","@PierreLel","@kdeniau","@_TerraInc","@DominicArpin","@antoinfonteneau","@nanotousch","@jb_roch","@YaniKhezzar","@Anne_Bechet","@NCapart","@SamyBenNaceur","@Joumany","@Julietteraynal","@TGiraudet","@SaraTanit","@HappeFrederic","@antoinellorca","@michelpicot","@Sev_Ryne","@bobdobolino","@murdever","@YGrandmontagne","@Mnyo","@EdKOSCIANSKI","@tnoisette","@jankari","@delbello_rom","@rflechaux","@NadiaSorelli","@IT_Digital","@abarbaux","@PhilippeLeroy","@schaptal","@marionspee","@lisavignoli","@ChloeAeberhardt","@MartineJacot","@JuliaPascualita","@curieusedetout","@sgraveleau","@bif_o","@ElisaPineau","@zinebdryef","@apiquard","@pierrehaski","@StephanieDelmas","@Blandine_Garot","@vergara_i","@evan_lebastard","@SophieVclt","@OlivierLevrault","@alicedorgeval","@LouiseMalnoy","@alix_fx","@pierre_baudis","@LucMagoutier","@AgatheMuller","@SGianninelli","@PaulineBoyer33","@NaomiHalll","@romaindlx","@marionbr","@Burtschy","@JacobEtienne","@as_lizzani","@marie_simon","@LaureDaussy","@FabriceAmedeo","@min","@LoubnaChlaikhy","@PlummerWilliam","@OlivierMarin1","@alaurefremont","@mwesfreid","@ChBaDe","@pmathon","@theobaldmarie","@Lnpagesy","@marclandre","@paoliniesther","@Feertchak","@JBLitzler","@GuillaumeErrard","@quentinperinel","@TristanQM","@mlbo","@constancejamet","@LoraTerrazas","@emiliegeffray","@Mathilde_Sd","@CaroPiquet","@DCanivez","@TIM_7375","@blandinelc","@ivanrioufol","@arthurberdah","@SarahLecoeuvre","@guillaume_gui","@DamienMercereau","@W_Chloe","@Assma_MD","@EugenieBastie","@HiTech_lexpress","@bcondominas","@Laurie_Z_","@jeanfrancgerard","@MathieuPagura","@BGUYF","@AlainPiffaretti","@AudreyKucinskas","@julienhory","@Pierrefalga","@TiphThuillier","@cdaniez","@LigerBaptiste","@D_Peras","@julie_dlb","@Fatiha_Temmouri","@julian2lamancha","@GaetaneDeljurie","@JulianMattei","@M_Vicuna","@DeBruynOlivier","@Nehed_Jendoubi","@antoine_grenapi","@ColonnaGen","@VictoriaGairin","@Clement_Lacombe","@TVigoureux","@MargauxObriot","@solinedelos","@RocheSabine","@dangerkens","@EdouardDutour","@MDondeyne","@DupuisNathalie1","@bouscarel","@Mathieu2jean","@Sophie_T_J","@laurentcalixte","@patrockwilliams","@PascaleKremer","@AlexJaquin","@LauraIsaaz","@cath_robin","@Del_Gautherin","@Isaduriez","@lucietuile","@AugeyBastien","@mcastagnet","@AminaKalache","@mvaudano","@CParrot","@ombelinetips","@_JoinLion","@BarbolosiRose","@ToiBruno1","@FloraClodic","@xjbdx","@ceci_est_un_handle_vraiment_long","@AlexiaEy","@Emjy_STARK","@elcoco01","@rabilebon","@pflovens_","@FabriceFrossard","@MorganeSERRES","@MarjolaineKoch","@edgarsnow","@SRNLF","@CChassigneux","@KerinecMoran","@NassiraELM","@NewsRicard","@Sandreene","@Emilezrt","@Pierre_Do","@Micode","@CColumelli","@DavidAbiker","@ClementBergantz","@benjaminrabier","@celinekallmann","@edwyplenel","@C_Barbier","@JJBourdin_RMC","@LaurenceFerrari","@aslapix","@IsaMillet","@MaximeSwitek","@tomjoubert","@jszanchi","@roqueeva","@XavierBiseul","@florencesantrot","@AntoineCrochet","@freeman59","@MaudeML","@philippe_gulpi","@mathieum76","@kiouari","@imanemoustakir","@BenedicteMallet","@Emilie_Brouze","@antoinebarret","@_nicolasbocquet","@remibuhagiar","@CourretB","@AymericRobert","@miraelmartins","@pmaniere","@jesuisraphk","@David_Ingram","@pcelerier","@technomedia","@Geraldinedauver","@ThierryLabro","@Newsdusud","@nrauline","@gbregeras","@SCouasnonBFM","@actualites_nrv","@dimitrimoulins","@oli_aura","@FabieChiche","@Vincent_Raimblt","@ChristophGreuet","@PAlbuchay","@MarrauddesGrot","@vtalmon","@cedric","@olivierfrigara","@Julien_Jay","@LydiaBerroyer","@Shuua","@datisdaz","@Steuph","@ameliecharnay","@Bruno_LesNums","@LelloucheNico","@CciliaDiQuinzio","@Elodie_C","@SylvRolland","@Kocobe","@FL_Debes","@jdupontcalbo","@GarciaVictor_","@NicoRichaud","@RHoueix","@simottel","@DamienLicata","@annabelle_L","@Lea_Lejeune","@axel_deb","@marin_eben","@ptiberry","@MatthieuDelach","@sandrinecassini","@benjaminferran","@ppgarcia75","@NotPatrick","@ivalerio","@FabienneSchmitt","@alexgoude","@JeromeColombain","@manhack","@Capucine_Cousin","@Fsorel","@oliviertesquet","@marjoriepaillon","@ginades","@PierreTran","@DelphineCuny","@reesmarc","@lauratenoudji","@ldupin","@carolinedescham","@Lucile_Quillet","@cgabizon","@Allocab","@epenser","@JAGventeprivee","@frwrds","@Laure__Bourdon","@Xavier75","@maximeverner","@s_jourdain","@LoriHelloc"]
+  return journalists
+end 
+
+# Combien y a-t-il de handle dans cette array ?
+
+def journalist_count(journalists)
+  puts "Combien il y a-t-il de handle Twitter dans notre base de données ? Cliquez sur la touche entrer pour initiez la requête."
+  initiate = gets.chomp
+  puts "> Il y a #{journalists.count} handle Twitter dans notre base de données."
+  puts " "
+end
+
+# Quel est le handle le plus court de cette liste ?
+
+def small_handle(journalists)
+  puts "Quel handle Twitter est le plus court dans notre base de données ? Cliquez sur la touche entrer pour initiez la requête."
+  initiate = gets.chomp
+  result = journalists.min {|a, b| a.length <=> b.length}
+  puts "> L'handle Twitter le plus court est #{result}."
+  puts " "
+end
+
+# Combien y-a-t'il de handle contenant 5 caractères (le @ ne compte pas pour un caractère)
+
+def journalist_count_5min(journalists)
+  puts "Combien il y a-t-il de handle Twitter dans notre base de données contenant 5 caractères ? Cliquez sur la touche entrer pour initiez la requête."
+  initiate = gets.chomp
+  journalists.to_s
+  result = journalists.select {|word| word.length == 6}.length
+  puts "> Il y a #{result} handle Twitter dans notre base de données contenant 5 caractères."
+  puts " "
+end
+
+# Combien commencent par une majuscule (première lettre juste après le @) ?
+
+def journalist_count_maj(journalists) 
+  puts "Combien il y a-t-il de handle Twitter dans notre base de données qui commencent par une majuscule ? Cliquez sur la touche entrer pour initiez la requête."
+  initiate = gets.chomp
+  result = journalists.grep(/@[A-Z]/).length
+  puts "> Il y a #{result} handle Twitter dans notre base de données commençant par une majuscule."
+  puts " "
+end
+
+# Trie la liste de handle par ordre alphabétique.
+
+def journalist_sort(journalists)
+  puts "Souhaitez vous trier la base de données par ordre alphabétique ? Cliquez sur la touche entrer pour initiez la requête."
+  initiate = gets.chomp
+  result = journalists.map(&:downcase).sort
+  puts "Base de données triées par ordre alphabétique : #{result}"
+  puts " "
+end
+
+# Trie la liste de handle par taille des handle (les plus petits en premiers, les plus grands après)
+
+def journalist_sort_by_length(journalists)
+  puts "Souhaitez vous trier la base de données par taille de l'handle Twitter ? Cliquez sur la touche entrer pour initiez la requête."
+  initiate = gets.chomp
+  result = journalists.sort_by {|word| word.length}
+  puts "Base de données triées par taille de l'handle Twitter : #{result}"
+  puts " "
+end
+
+# Quelle est la position dans l'array de la personne @epenser ?
+
+def position(journalists)
+  puts "Quelle est la position dans la base de données de l'handle Twitter @epenser ? Cliquez sur la touche entrer pour initiez la requête."
+  initiate = gets.chomp
+  result = journalists.find_index("@epenser")
+  puts "@epenser se trouve en position #{result} dans la base de données."
+  puts " "
+end
+
+#Sors-moi une répartition des handle par taille de ces derniers (nombre de handle avec 1 caractère, nombre de handle avec 2 caractères, nombre de handle avec 3 caractères, etc)
+
+def length_frequency(journalists)     
+  handle_length_frequency = Hash.new(0)
+  for i in 0...journalists.length    
+    handle_length_frequency[journalists[i].length] +=  1
+  end
+  puts "Souhaites tu une répartition des handles de la base de données en fonction de leurs nombre de caractères ? Cliquez sur la touche entrer pour initiez la requête."
+  initiate = gets.chomp
+  a = 1
+  for i in 1..33
+    if a == 1 or a == 2 or a == 3 or a == 17 or a == 18 or a == 19 or a == 20 or a == 21  or a == 22 or a == 23 or a == 24 or a == 25 or a == 26 or a == 27 or a == 28 or a == 29 or a == 30 or a == 31 or a == 32
+      puts "Il y a #{handle_length_frequency.default} handles Twitter avec #{i} caractères"
+    else 
+      puts "Il y a #{handle_length_frequency.fetch(a)} handles Twitter avec #{i} caractères"
+    end
+    a += 1
+    i += 1
+  end
+end  
+
+def perform
+  journalist_count(data)
+  small_handle(data)
+  journalist_count_5min(data)
+  journalist_count_maj(data)
+  journalist_sort(data)
+  journalist_sort_by_length(data)
+  position(data)
+  length_frequency(data)
+end
+
+perform
